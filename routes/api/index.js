9afa86224035d9f4122e2c6e84dd697e417a21af
akaur091 <amniderkaur.09@gmail.com>
2022-05-23 23:06:11 -0700

    add reoutes requirer in api index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,11 @@
 // Require Express
-
+const router = require('express').Router();
 // Require Routes
-
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 //  Add user to beginning of user routes
-
+router.use('/users', userRoutes);
 // Add thoughts to beginning of user routes
+router.use('./thought', thoughtRoutes);
+
+module.exports = router;
